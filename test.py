d77c013ac8d32802b0a99d4c06b9fd3ef6a53862
farhadi-abz <aboozarfarhadi2016@gmail.com>
2025-09-05 17:10:42 +0330

    remove some garbage

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from rich import print
-
-
-def print_name(video_path):
-    srt_filename: str = os.path.splitext(video_path)[0] + ".srt"
-    srt_filename = srt_filename.replace("\\", "/")
-    print(srt_filename)
